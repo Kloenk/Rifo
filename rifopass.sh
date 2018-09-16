4f1a794b99b5c7c055d6688da55f65299da63001
Finn Behrens <finn@trudeltiere.de>
2018-09-16 22:33:22 +0200

    implement passlistgen into rifopass.sh
    Signed-off-by: Finn Behrens <finn@trudeltiere.de>

--- rifopass.sh
+++ rifopass.sh
@@ -9,7 +9,32 @@
 # Requirements: termite fzf jq pass
 #
 
-CACHE=~/.local/share/rifopass-cache
+passlistgen()
+{
+	shopt -s nullglob globstar
+	prefix=${PASSWORD_STORE_DIR-~/.password-store}
+	password_files=( "$prefix"/**/*.gpg )
+	password_files=( "${password_files[@]%.gpg}" )
+	password_files=( "${password_files[@]#"$prefix"/}" )
+	printf '%s\n' "${password_files[@]}"
+	shopt -u nullglob globstar
+}
+
+decodeName()
+{
+	if [ $(echo $1 | head -1) = '/' ] & [ -e $1 ]; then
+		echo $1
+	else 
+		file=$HOME/$1
+		if [ -e $file ]; then 
+			echo $file
+		else
+			exit 1
+		fi
+	fi
+}
+
+CACHE=$HOME/.local/share/rifopass-cache
 
 #
 # Code starts here
@@ -17,17 +42,17 @@ CACHE=~/.local/share/rifopass-cache
 
 args=foo
 
-if [ $1 == "fill-in" ]; then
+if [ $1 = "fill-in" ]; then
 	args=fill_in
 	shift
 fi
 
 if [ -z "$1" ]
 then
-termite --title="rifopass" --exec="bash -c '~/.rifopass.sh $args > ~/.rifopass-command'"
+	termite --title="rifopass" --exec="bash -c '$(decodeName $0) $args > /tmp/rifopass-command'"
 # run the application
-bash ~/.rifopass-command
-
+bash -c /tmp/rifopass-command
+rm /tmp/rifopass-command
 exit 0
 fi
 
@@ -35,7 +60,7 @@ fi
 touch "$CACHE"
 
 # join together the full list of commands and cache file
-LIST=$(join -a2 <(sort "$CACHE") <( ( ~/.passlistgen.sh | sed 's/ /_/g') | sort -u) | sort -Vr -k2 | cut -d' ' -f1)
+LIST=$(join -a2 <(sort "$CACHE") <( ( passlistgen | sed 's/ /_/g') | sort -u) | sort -Vr -k2 | cut -d' ' -f1)
 
 # fzf prompt
 SEL=$(echo "$LIST" | sed 's/_/ /g' | fzf --no-sort | sed 's/ /_/g')
@@ -56,7 +81,7 @@ sed "s/^$SEL .*$/$SEL $((COUNT+1))/" -i "$CACHE"
 fi
 
 # return the selected application
-if [ $1 == "fill_in" ]; then
+if [ $1 = "fill_in" ]; then
 	echo pass $(echo $SEL | sed 's/_/\\ /g') '| { IFS= read -r pass; printf %s "$pass"; } | xdotool type --clearmodifiers --file -'
 else
 	echo pass -c $(echo $SEL | sed 's/_/\\ /g')
