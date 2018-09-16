118ac1a5dcadbc6adb70a2fc60c679ba693ce8c9
Finn Behrens <finn@trudeltiere.de>
2018-09-16 22:30:33 +0200

    undo shopt options in passlistgen.sh
    Signed-off-by: Finn Behrens <finn@trudeltiere.de>

--- passlistgen.sh
+++ passlistgen.sh
@@ -5,3 +5,4 @@ password_files=( "$prefix"/**/*.gpg )
 password_files=( "${password_files[@]%.gpg}" )
 password_files=( "${password_files[@]#"$prefix"/}" )
 printf '%s\n' "${password_files[@]}" 
+shopt -u nullglob globstar
